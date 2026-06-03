8f3d7dd945e034f960a675c9491afb4007aa2c61
tanpeirou <81800169+tanpeirou@users.noreply.github.com>
2026-06-03 17:46:58 +0900

    Update resource_monitor.py

--- resource_monitor.py
+++ resource_monitor.py
@@ -1,24 +1,17 @@
 import psutil
 import time
+import os
 
 class ResourceMonitor:
-
     def start(self):
-
-        self.process = psutil.Process()
-
+        self.process = psutil.Process(os.getpid())
         self.start_mem = self.process.memory_info().rss
-
         self.start_time = time.time()
 
     def stop(self):
-
         end_mem = self.process.memory_info().rss
-
         elapsed = time.time() - self.start_time
-
         return {
             "latency": elapsed,
-            "memory_mb":
-            (end_mem - self.start_mem)/(1024*1024)
+            "memory_mb": (end_mem - self.start_mem) / (1024 * 1024)
         }
